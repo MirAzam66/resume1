0b3cae5ebcf3a6493e9b7289841a4875756e01c9
Mir Azam <168904987+MirAzam66@users.noreply.github.com>
2024-05-23 03:55:42 +0500

    add js

---
+++ index.js
@@ -0,0 +1,9 @@
+function chng1(){
+    document.getElementById("ch1").src="logo.jpg"
+}
+function chng2(){
+    document.getElementById("ch2").src="logo.jpg"
+}
+function chng3(){
+    document.getElementById("ch3").src="logo.jpg"
+}
